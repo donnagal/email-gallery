1374e64efa77dfd4c6431a647d52175380ae34a7
donnagalletta <donrgalletta@gmail.com>
2020-12-03 13:31:06 +1100

    EE-1794 Update gallery to use same data as repot

--- js/filter-group.js
+++ js/filter-group.js
@@ -2,25 +2,36 @@ $(function() {
   $(".btn-brand").click(function(){
     $(".card-brand").toggleClass('hidden');
     $(this).toggleClass('bg-red');
+    $(this).toggleClass('bg-green');
     $(".btn-brand .fa-times-circle").toggleClass('hidden');
     $(".btn-brand .fa-check-circle").toggleClass('hidden');
   });
   $(".btn-comms").click(function(){
     $(".card-comms").toggleClass('hidden');
     $(this).toggleClass('bg-red');
+    $(this).toggleClass('bg-green');
     $(".btn-comms .fa-times-circle").toggleClass('hidden');
     $(".btn-comms .fa-check-circle").toggleClass('hidden');
   });
   $(".btn-member").click(function(){
     $(".card-member").toggleClass('hidden');
     $(this).toggleClass('bg-red');
+    $(this).toggleClass('bg-green');
     $(".btn-member .fa-times-circle").toggleClass('hidden');
     $(".btn-member .fa-check-circle").toggleClass('hidden');
   });
   $(".btn-webinar").click(function(){
     $(".card-webinar").toggleClass('hidden');
     $(this).toggleClass('bg-red');
+    $(this).toggleClass('bg-green');
     $(".btn-webinar .fa-times-circle").toggleClass('hidden');
     $(".btn-webinar .fa-check-circle").toggleClass('hidden');
+  });
+  $(".btn-template").click(function(){
+    $(".card-template").toggleClass('hidden');
+    $(this).toggleClass('bg-red');
+    $(this).toggleClass('bg-green');
+    $(".btn-template .fa-times-circle").toggleClass('hidden');
+    $(".btn-template .fa-check-circle").toggleClass('hidden');
   });
 });
