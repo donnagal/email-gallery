4bf9abe4ec5fbbf1b7826f201f55de6cedeef6e1
donnagalletta <donrgalletta@gmail.com>
2021-03-10 16:38:07 +1100

    vue-sort-table with json

---
+++ demo/script.js
@@ -0,0 +1,32 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    users: [],
+    
+    sortKey: null,
+    reverse: false,
+  },
+  computed: {},
+  methods: {
+    sortBy(sortKey) {
+      this.users.sort( (a, b) => {
+        //  numeric sort
+        if(typeof a[sortKey] === 'number'){
+          return a[sortKey] - b[sortKey];
+        }
+        //  alphanumeric sort 
+        const A = a[sortKey].toUpperCase();
+        const B = b[sortKey].toUpperCase();                
+        return A < B ? -1 : A > B ?  1 : 0;
+      });
+      this.reverse = (this.sortKey === sortKey) ? !this.reverse : false;
+      if(this.reverse) this.users.reverse();
+      this.sortKey = sortKey;
+    }
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(resp => resp.json())
+      .then(json => (this.users = json));
+  }
+});
