ac506599c7a4c75f62048f121b59bf8e903b482e
Donna Galletta <donrgalletta@gmail.com>
2024-07-22 15:45:17 +1000

    cont. icon updates

--- pages/reports/script_icons.js
+++ pages/reports/script_icons.js
@@ -32,3 +32,26 @@ const vm = new Vue({
       .then(json => (this.lists = json));
   }
 });
+
+
+// Force download image to folder
+function forceDownload(link){
+  var url = link.getAttribute("data-href");
+  var fileName = link.getAttribute("download");
+  // link.innerText = "Working...";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "blob";
+  xhr.onload = function(){
+      var urlCreator = window.URL || window.webkitURL;
+      var imageUrl = urlCreator.createObjectURL(this.response);
+      var tag = document.createElement('a');
+      tag.href = imageUrl;
+      tag.download = fileName;
+      // document.body.appendChild(tag);
+      tag.click();
+      // document.body.removeChild(tag);
+      // link.innerText="Download Image";
+  }
+  xhr.send();
+}
