949ac3b06eb0718219da1a1abc23fa06e6d9fc7d
Donna Galletta <donrgalletta@gmail.com>
2024-12-11 16:36:38 +1100

    add tooltip

--- pages/reports/script_icons.js
+++ pages/reports/script_icons.js
@@ -34,25 +34,15 @@ const vm = new Vue({
   }
 });
 
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
 
-// Force download image to folder
-function forceDownload(link){
-  var url = link.getAttribute("data-href");
-  var fileName = link.getAttribute("download");
-  // link.innerText = "Working...";
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.responseType = "blob";
-  xhr.onload = function(){
-      var urlCreator = window.URL || window.webkitURL;
-      var imageUrl = urlCreator.createObjectURL(this.response);
-      var tag = document.createElement('a');
-      tag.href = imageUrl;
-      tag.download = fileName;
-      document.body.appendChild(tag);
-      tag.click();
-      // document.body.removeChild(tag);
-      // link.innerText="Download Image";
-  }
-  xhr.send();
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
 }
