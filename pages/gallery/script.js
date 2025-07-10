b39c613f7731966165c19c8d4675826e712bb6a4
Donna Galletta <donrgalletta@gmail.com>
2025-07-10 12:10:39 +1000

    cont updates with update home page.

--- pages/gallery/script.js
+++ pages/gallery/script.js
@@ -1,32 +1,14 @@
-const vm = new Vue({
-  el: "#app",
-  data: {
-    lists: [],
-
-    sortKey: null,
-    reverse: false,
-  },
-  computed: {},
-  methods: {
-    sortBy(sortKey) {
-      this.lists.sort((a, b) => {
-        //  numeric sort
-        if (typeof a[sortKey] === "number") {
-          return a[sortKey] - b[sortKey];
-        }
-        //  alphanumeric sort
-        const A = a[sortKey].toUpperCase();
-        const B = b[sortKey].toUpperCase();
-        return A < B ? -1 : A > B ? 1 : 0;
-      });
-      this.reverse = this.sortKey === sortKey ? !this.reverse : false;
-      if (this.reverse) this.lists.reverse();
-      this.sortKey = sortKey;
+  new Vue({
+    el: "#app",
+    data: {
+      imageCount: 28, // Update to match the number of images
     },
-  },
-  mounted() {
-    fetch("./gallery.json")
-      .then((resp) => resp.json())
-      .then((json) => (this.lists = json));
-  },
-});
+    mounted() {
+      lightGallery(this.$refs.lightGallery, {
+        plugins: [lgZoom], 
+        speed: 500,
+        thumbnail: false, 
+        zoom: true,
+      });
+    },
+  });
